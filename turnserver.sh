8895c6aa0b539203768f4b6e17a4bb327581b817
koori69 <koori69@users.noreply.github.com>
2018-08-06 10:08:09 +0800

    Create turnserver.sh

---
+++ turnserver.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -z $PORT ]
+then
+    PORT=3478
+fi
+
+if [ -z $REALM ]
+then
+    REALM=inovance.com
+fi
+
+if [ -z $USER ]
+then
+    USER=inosa:inosa1234
+fi
+
+
+echo log-file=/var/tmp/turn.log > /etc/turnserver/turnserver.conf
+echo pidfile="var/run/turnserver/turnserver.pid" >> /etc/turnserver/turnserver.conf
+echo no-stdout-log >> /etc/turnserver/turnserver.conf
+echo realm=$REALM >> /etcturnserver//turnserver.conf
+echo user=$USER >> /etc/turnserver/turnserver.conf
+echo external-ip=$EXTERNAL_IP >> /etc/turnserver/turnserver.conf
+echo listening-port=$PORT >> /etc/turnserver/turnserver.conf
+
+turnserver -v -r $EXTERNAL_IP -a  -c /etc/turnserver/turnserver.conf 
